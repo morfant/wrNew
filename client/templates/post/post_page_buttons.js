9c8b099fa96ab842cbd2b41509d7691c2bd30e67
Gangil <git>
2016-07-14 22:40:39 +0900

    Done category button selection.

--- client/templates/post/post_page_buttons.js
+++ client/templates/post/post_page_buttons.js
@@ -1,10 +1,5 @@
 Template.postButtons.created = function(){
-    console.log(this)
-    console.log(this.lastNode);
-    console.log(this.data);
-    console.log(this.firstNode);
-
-
+    // console.log(this.data.isOnNow);
 
 };
 
@@ -14,35 +9,66 @@ Template.postButtons.events({
 
         var btnVal = $(e.target).attr("value");
 
-    //     Meteor.call('updatePostStatus', this._id, btnVal, true,
-    //       function(error, result) {
-    //         if (result) {
-    //           console.log(result);
-    //           template.find('#onNow').checked = true;
-    //         }
-    //       });
-    //       console.log(btnVal + " button click!");
-    // }
+        var onNow_IsChecked = template.find('#onNow').checked;
+        var upNext_IsChecked = template.find('#upNext').checked;
+        var lastEp_IsChecked = template.find('#lastEp').checked;
 
+        var onNow_setVal = false;
+        var upNext_setVal = false;
+        var lastEp_setVal = false;
 
-     Meteor.call('updatePostStatus', this._id, btnVal, true,
-          function(error, result){ //callback of Meteor.call()
-            if (result) {
-              if (btnVal == "onNow"){
-                template.find('#onNow').checked = true;
-              }
-              else if (btnVal == "upNext")
-                template.find('#upNext').checked = true;
-              else if (btnVal == "lastEp")
-                template.find('#lastEp').checked = true;
-            }
-          });
+        var setVal = {
+            onNow: false,
+            upNext: false,
+            lastEp: false
+        };
 
-        console.log(btnVal + " button click!");
+        if (onNow_IsChecked) setVal.onNow = false;
+        else setVal.onNow = true;
+
+        if (upNext_IsChecked) setVal.upNext = false;
+        else setVal.upNext = true;
+
+        if (lastEp_IsChecked) setVal.lastEp = false;
+        else setVal.lastEp = true;
+
+        // console.log(btnVal + ": " + setVal[btnVal]);
+
+        Meteor.call('updatePostStatus', this._id, btnVal, setVal[btnVal],
+            function(error, result){ //callback of Meteor.call()
+                // console.log("result: " + result);
+                if (result) {
+                  if (btnVal == "onNow"){
+                    template.find('#onNow').checked = setVal[btnVal];
+                    template.find('#upNext').checked = false;
+                    template.find('#lastEp').checked = false;
+                  } else if (btnVal == "upNext") {
+                    template.find('#onNow').checked = false;
+                    template.find('#upNext').checked = setVal[btnVal];
+                    template.find('#lastEp').checked = false;
+                  } else if (btnVal == "lastEp") {
+                    template.find('#onNow').checked = false;
+                    template.find('#upNext').checked = false;
+                    template.find('#lastEp').checked = setVal[btnVal];
+                  }
+                }
+              });
+
+        // console.log(btnVal + " button click!");
+
     }
 
 
 
 });
 
-   
+Template.postButtons.rendered = function(){
+    // console.log(this.data.isOnNow);
+
+    this.find('#onNow').checked = this.data.isOnNow;
+    this.find('#upNext').checked = this.data.isUpNext;
+    this.find('#lastEp').checked = this.data.isLastEp;
+
+};
+
+
--- server/fixture.js
+++ server/fixture.js
@@ -1,25 +1,25 @@
-if (Posts.find().count() === 0) {
-  Posts.insert({
-    title: '#1',
-    contents: 'hello',
-    isOnNow: true,
-    isUpNext: false,
-    isLasts: false
-  });
+// if (Posts.find().count() === 0) {
+//   Posts.insert({
+//     title: '#1',
+//     contents: 'hello',
+//     isOnNow: true,
+//     isUpNext: false,
+//     isLasts: false
+//   });
 
-  Posts.insert({
-    title: '#2',
-    contents: 'http://jjwc.cafe24.com:8000/mpd.ogg',
-    isOnNow: false,
-    isUpNext: true,
-    isLasts: false
-  });
+//   Posts.insert({
+//     title: '#2',
+//     contents: 'http://jjwc.cafe24.com:8000/mpd.ogg',
+//     isOnNow: false,
+//     isUpNext: true,
+//     isLasts: false
+//   });
 
-  Posts.insert({
-    title: '#3',
-    contents: 'http://jjwc.cafe24.com:8000/mpd.ogg',
-    isOnNow: false,
-    isUpNext: false,
-    isLasts: true 
-  });
-}
+//   Posts.insert({
+//     title: '#3',
+//     contents: 'http://jjwc.cafe24.com:8000/mpd.ogg',
+//     isOnNow: false,
+//     isUpNext: false,
+//     isLasts: true 
+//   });
+// }
