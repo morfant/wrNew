e940906d73e757b214f2860d6d60494703d17a47
Gangil <git>
2016-07-21 14:23:44 +0900

    SNS sharing edit.

--- client/templates/application/startup.js
+++ client/templates/application/startup.js
@@ -1,3 +1,14 @@
 Meteor.startup(function() {
-
+ if(Meteor.isClient){
+    console.log("start up!");
+      return SEO.config({
+        title: 'weather report',
+        meta: {
+          'description': 'Artist run internet radio'
+        },
+        og: {
+          'image': 'https://drive.google.com/uc?export=view&id=0B5O0D-88dhuVcHd6UDh3UmNORE0' 
+        }
+      });
+    }
 });
