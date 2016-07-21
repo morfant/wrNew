83e99649aedd2c469a1e01be8620c54c57ade334
Gangil <git>
2016-07-21 15:55:49 +0900

    Google webmaster site verified.

--- client/templates/application/startup.js
+++ client/templates/application/startup.js
@@ -1,14 +1,3 @@
 Meteor.startup(function() {
- if(Meteor.isClient){
-    console.log("start up!");
-      return SEO.config({
-        title: 'weather report',
-        meta: {
-          'description': 'Artist run internet radio'
-        },
-        og: {
-          'image': 'https://drive.google.com/uc?export=view&id=0B5O0D-88dhuVcHd6UDh3UmNORE0' 
-        }
-      });
-    }
+
 });
