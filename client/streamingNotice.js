93dfb18b445bb454a58649bcc997057ddacef89c
giy <giy@giyui-MacBook-Pro.local>
2017-02-01 12:03:26 +0900

    Seperate global function to a file

--- client/streamingNotice.js
+++ client/streamingNotice.js
@@ -7,34 +7,6 @@ var isLastEpExist = false;
 
 var checkStreamingStatusInterval = false;
 
-var checkStreamingStatus = function(){
-  var url = getStreamURL();
-  Meteor.call('checkStreamingOn', url, function(error, result) {
-    if (!error) {
-      // console.log(result);
-      // var json = JSON.parse(result);
-      // console.log(json);
-
-      // console.log(result.icestats.source);
-      // console.log(result);
-      // var strReady = result.includes(STREAMING_URL);
-      var strReady = result.includes(STREAMING_MOUNTPOINT);
-      // var streamSrc = result.icestats.source;
-      // if (_.isEmpty(streamSrc)){
-      if (strReady) {
-        Session.set('streamReady', true);
-      } else {
-        Session.set('streamReady', false);
-      }
-    } else {
-      console.log(error);
-    }
-    // var userId = Meteor.userId();
-    // if (userId) console.log("checkStreamingStatus() - streamReady : " + Session.get('streamReady'));
-    // console.log("checkStreamingStatus() - streamReady : " + Session.get('streamReady'));
-  });
-};
-
 Template.streamingNotice.created = function() {
   onNowPost = Posts.findOne({isOnNow: true});
   upNextPost = Posts.findOne({isUpNext: true});
