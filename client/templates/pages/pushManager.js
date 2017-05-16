79787654dc46e38d7772e6b79aed7db25804c4d9
morfant <giy.hands@gmail.com>
2017-05-16 22:15:48 +0900

    Succeed push sending on web page.

--- client/templates/pages/pushManager.js
+++ client/templates/pages/pushManager.js
@@ -1,5 +1,5 @@
 Template.pushManager.created = function() {
-    
+
 };
 
 
@@ -8,7 +8,32 @@ Template.pushManager.helpers({
 });
 
 Template.pushManager.events({
+  'submit form': function(e, template) {
+    console.log("click push send");
+    e.preventDefault();
 
+    var title = $(e.target).find('#pushTitle').val()
+    var content = $(e.target).find('#pushContent').val()
+    // console.log(title);
+    // console.log(content);
+
+    if (confirm("이대로 push 메시지를 발송하시겠습니까?") == true){
+        Meteor.call('sendPush', title, content, function(error, result) {
+
+          // display the error to the user and abort
+          if (error)
+            return throwError(error.reason);
+          if (result){
+            console.log(result);
+            $(e.target).find('#pushTitle').val('');
+            $(e.target).find('#pushContent').val('');
+          }
+
+        });
+    } else {
+
+    }
+  }
 
 });
 
